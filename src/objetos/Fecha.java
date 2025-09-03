b81ad7439bfd8fe3665acc402c706caafd160c8d
Andrescaliva <andrescaliva25@gmail.com>
2025-09-03 14:56:15 -0300

    ejercicios Fecha

--- src/objetos/Fecha.java
+++ src/objetos/Fecha.java
@@ -18,6 +18,11 @@ public class Fecha {
 		System.out.println(this.dia+"/"+this.mes+"/"+this.anio);
 	}
 	
+	@Override
+	public String toString() {
+		return this.dia+"/"+this.mes+"/"+this.anio;
+	}
+
 	public static boolean bisiesto(int anio) {
 		if(anio%4==0 && anio%100!=0) {
 			return true;
@@ -26,6 +31,51 @@ public class Fecha {
 			return true;
 		}
 		return false;
+	}
+	
+	
+	public static int diasDelMes(int mes, int anio){
+		if(mes==1||mes==3||mes==5||mes==7||mes==8||mes==10||mes==12) {
+			return 31;
+		}
+		else  if(mes==4||mes==6||mes==9||mes==11){
+			return 30;
+		}
+		else if(mes==2 && bisiesto(anio)){
+			return 29;
+		}
+		else {
+			return 28;
+		}
+	}
+	
+	public boolean esValida() {
+		if(anio<0) {
+			return false;
+		}
+		if(mes<1||mes>12) {
+			return false;
+		}
+		if(dia<1||dia>diasDelMes(mes,anio)) {
+			return false;
+		}
+		return true;
+	
+	}
+	
+	public void avanzarDia() {
+		if(this.dia==diasDelMes(this.anio,this.mes)) {
+			this.dia=1;
+			if(this.mes==12) {
+				this.mes=1;
+				this.anio++;
+			}else{
+				this.mes++;
+			}
+		}
+		else {
+			this.dia++;
+		}
 	}
 
 }
--- src/objetos/Main.java
+++ src/objetos/Main.java
@@ -5,17 +5,18 @@ import java.awt.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Point p1 =new Point(15,5);
-		//Point p2 = new Point(3,4);
+		/*Point p1 =new Point(15,5);
+		Point p2 = new Point(3,4);
 		Rectangle caja = new Rectangle(10,10,40,20);
 		Rectangle caja2 = new Rectangle(20,5,20,20);
 		Objetos.mover(caja, 50, 50);
-		//System.out.println("La distancia entre los dos puntos es de: "+Objetos.distancia(p1, p2));
+		System.out.println("La distancia entre los dos puntos es de: "+Objetos.distancia(p1, p2));
 		System.out.println(Objetos.estaDentro(p1, caja));
-		System.out.println(Objetos.encuadrar(caja, caja2));
-		Fecha f= new Fecha(2,9,2025);
+		System.out.println(Objetos.encuadrar(caja, caja2));*/
+		Fecha f= new Fecha(31,12,2025);
 		f.imprimir();
-		System.out.println(Fecha.bisiesto(2025));
+		f.avanzarDia();
+		f.imprimir();
 
 	}
 
